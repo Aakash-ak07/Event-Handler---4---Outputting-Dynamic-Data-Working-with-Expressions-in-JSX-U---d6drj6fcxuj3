e7ac01ede430484708fb685b0ad5b73c322f1a26
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-19 14:23:40 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
-  const handleDoubleClick = (event) =>{
   
+  const handleDoubleClick = (event) =>{
+    if(event.detail === 2){
+      console.log("I was double clicked");
+    }
+    else{
+      console.log("I was not double clicked");
+    }
   }
   return (
     <div id="main">
